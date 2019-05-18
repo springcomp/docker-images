3a946c9ba47a98fc5757ac5dc9f7dbaf9f44ea8f
Maxime Labelle <maxime.labelle@moskitos.fr>
2019-05-18 09:01:16 +0200

    Downgrade Windows Version to support a wider range of platforms.

--- msklc/Make-DockerContext.ps1
+++ msklc/Make-DockerContext.ps1
@@ -14,6 +14,7 @@ BEGIN{
 
 PROCESS{
 
+    New-Item -Path $context -ItemType Directory -EA SilentlyContinue | Out-Null
     Copy-Item -Path "C:\Program Files (x86)\Microsoft Keyboard Layout Creator 1.4\*" -Recurse -Force -Filter "*.*" -Destination $context
 
 }
